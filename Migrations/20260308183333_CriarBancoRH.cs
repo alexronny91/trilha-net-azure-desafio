ad5538872ca15b778895f87b0c5d975245e79f5c
Alex Ronny <alexronny91@gmail.com>
2026-03-08 16:57:22 -0300

    Execução API via local ok

---
+++ Migrations/20260308183333_CriarBancoRH.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trilha_net_azure_desafio.Migrations
+{
+    public partial class CriarBancoRH : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
